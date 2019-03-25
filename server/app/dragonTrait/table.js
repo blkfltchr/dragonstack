4b5e42583cb234776c7eb93f7d58053d1fa5a07f
Blake Fletcher <blkfltchr@gmail.com>
2019-03-25 14:26:10 -0400

    Cannot read property 'id' of undefined

---
+++ server/app/dragonTrait/table.js
@@ -0,0 +1,26 @@
+const pool = require('../../databasePool');
+const TraitTable = require('../trait/table');
+
+class DragonTraitTable {
+  // Side effect and not return value, so no promise necessary.
+  // But the promise keeps things consistent with the other table functions.
+  static storeDragonTrait({ dragonId, traitType, traitValue }) {
+    return new Promise((resolve, reject) => {
+      TraitTable.getTraitId({ traitType, traitValue })
+        .then(({ traitId }) => {
+          pool.query(
+            'INSERT INTO dragonTrait("traitId", "dragonId") VALUES($1, $2)',
+            [traitId, dragonId],
+            (error, response) => {
+              if (error) return reject(error);
+
+              resolve();
+            }
+          );
+        })
+        .catch(error => reject(error));
+    });
+  }
+}
+
+module.exports = DragonTraitTable;
--- server/bin/configure_db.sh
+++ server/bin/configure_db.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 export PGPASSWORD="node_password"
 
 echo "Configuring dragonstackdb"
@@ -9,5 +8,9 @@ createdb -U node_user dragonstackdb
 
 psql -U node_user dragonstackdb < ./bin/sql/generation.sql
 psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
+psql -U node_user dragonstackdb < ./bin/sql/trait.sql
+psql -U node_user dragonstackdb < ./bin/sql/dragonTrait.sql
+
+node ./bin/insertTraits.js
 
 echo "dragonstackdb configured"
